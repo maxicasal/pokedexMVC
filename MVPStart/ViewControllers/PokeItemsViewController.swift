c1bf955d11e0f082e80e3538e36375f4b65351e5
Максим Алексеев <mrvalter566@gmail.com>
2020-03-24 20:40:12 +0700

    Rewrite PokeItemsViewController

--- MVPStart/ViewControllers/PokeItemsViewController.swift
+++ MVPStart/ViewControllers/PokeItemsViewController.swift
@@ -9,38 +9,39 @@
 import UIKit
 
 class PokeItemsViewController: UIViewController {
-
-  private let kItemCellIdentifier = "kItemCellIdentifier"
-  private var items = [Item]()
-  @IBOutlet var itemsTableView: UITableView!
-
-  override func viewWillAppear(animated: Bool) {
-    super.viewWillAppear(animated)
-    loadItemsFromPlist()
-  }
-
-  private func loadItemsFromPlist() {
-    let itemsArray = NSArray(contentsOfFile: NSBundle.mainBundle().pathForResource("PokeItems", ofType: "plist")!)
-    for itemDict in itemsArray! {
-      items.append(Item(name: itemDict["Name"] as! String,
-        fullDescription: itemDict["Description"] as! String,
-        imageName: itemDict["ImageName"] as! String))
+    
+    private let kItemCellIdentifier = "kItemCellIdentifier"
+    private var items = [Item]()
+    @IBOutlet var itemsTableView: UITableView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadItemsFromPlist()
     }
-    itemsTableView.reloadData()
-  }
+    
+    private func loadItemsFromPlist() {
+        if  let path = Bundle.main.path(forResource: "PokeItems", ofType: "plist"),
+            let xml = FileManager.default.contents(atPath: path) {
+            guard let array = try? PropertyListSerialization.propertyList(from: xml, options: .mutableContainersAndLeaves, format: nil) as? [Item] else { return }
+            self.items = array
+            itemsTableView.reloadData()
+        }
+    }
 }
 
 extension PokeItemsViewController: UITableViewDataSource, UITableViewDelegate {
-
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return items.count
-  }
-
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let item = items[indexPath.row]
-    let cell = tableView.dequeueReusableCellWithIdentifier(kItemCellIdentifier, forIndexPath: indexPath) as! ItemTableViewCell
-    cell.configure(item)
-
-    return cell
-  }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = items[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: kItemCellIdentifier,for: indexPath) as! ItemTableViewCell
+        cell.configure(item: item)
+        
+        return cell
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
 }
