5f1c106e43a912bc550a97b3a83b95d6fa88fd0a
Максим Алексеев <mrvalter566@gmail.com>
2020-03-24 19:49:57 +0700

    Finally rewrite api manager

--- MVPStart/APIManager/APIManager.swift
+++ MVPStart/APIManager/APIManager.swift
@@ -10,40 +10,40 @@ import Foundation
 import Alamofire
 
 class APIManager {
-  let kBasePokemonURL = "http://pokeapi.co/api/v2/pokemon/"
-
-  struct Static {
-    static var instance: APIManager?
-  }
-
-  class var sharedInstance : APIManager {
-
-    if(Static.instance == nil) {
-      Static.instance = APIManager()
+    let kBasePokemonURL = "http://pokeapi.co/api/v2/pokemon/"
+    
+    struct Static {
+        static var instance: APIManager?
     }
-
-    return Static.instance!
-  }
-
-  class func trustAllCertificates() {
-    let manager = Alamofire.Manager.sharedInstance
-    manager.delegate.sessionDidReceiveChallenge = { session, challenge in
-      var disposition: NSURLSessionAuthChallengeDisposition = .PerformDefaultHandling
-      var credential: NSURLCredential?
-      if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
-        disposition = NSURLSessionAuthChallengeDisposition.UseCredential
-        credential = NSURLCredential(forTrust: challenge.protectionSpace.serverTrust!)
-      } else {
-        if challenge.previousFailureCount > 0 {
-          disposition = .CancelAuthenticationChallenge
-        } else {
-          credential = manager.session.configuration.URLCredentialStorage?.defaultCredentialForProtectionSpace(challenge.protectionSpace)
-          if credential != nil {
-            disposition = .UseCredential
-          }
+    
+    class var sharedInstance : APIManager {
+        
+        if(Static.instance == nil) {
+            Static.instance = APIManager()
         }
-      }
-      return (disposition, credential)
+        
+        return Static.instance!
     }
-  }
+    
+    class func trustAllCertificates() {
+        let manager = Alamofire.SessionManager()
+        manager.delegate.sessionDidReceiveChallenge = { session, challenge in
+            var disposition: URLSession.AuthChallengeDisposition = .performDefaultHandling
+            var credential: URLCredential?
+            if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
+                disposition = URLSession.AuthChallengeDisposition.useCredential
+                credential = URLCredential(trust: challenge.protectionSpace.serverTrust!)
+            } else {
+                if challenge.previousFailureCount > 0 {
+                    disposition = .cancelAuthenticationChallenge
+                } else {
+                    credential = manager.session.configuration.urlCredentialStorage?.defaultCredential(for: challenge.protectionSpace)
+                    if credential != nil {
+                        disposition = .useCredential
+                    }
+                }
+            }
+            return (disposition, credential)
+        }
+    }
 }
